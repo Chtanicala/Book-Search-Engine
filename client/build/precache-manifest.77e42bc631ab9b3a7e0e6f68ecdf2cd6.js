86f0d9fac79b12e677d51bb3f5621dcde0d3c6a2
Christian Tanicala <christian.tanicala@gmail.com>
2022-01-10 23:06:49 -0500

    resolved proxy error, but things still not showing up

---
+++ client/build/precache-manifest.77e42bc631ab9b3a7e0e6f68ecdf2cd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ecba751b426f1df71460c599b79138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40e2179fbe0043e7ef03",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "3ea8e9eac1881d7e3799",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "40e2179fbe0043e7ef03",
+    "url": "/static/js/2.d7c0ff9c.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.d7c0ff9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea8e9eac1881d7e3799",
+    "url": "/static/js/main.583be393.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
