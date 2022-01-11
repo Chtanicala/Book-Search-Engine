f45ce4d00b6f048364b69fe41dc4f80d53165b92
Christian Tanicala <christian.tanicala@gmail.com>
2022-01-10 20:06:02 -0500

    Started TypeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,45 @@
 const {gqp} = require('apollo-server-express');
 
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]!
+    }
+    
+    type Book {
+        _id: ID
+        authors: [String]!
+        description: String!
+        bookId: ID!
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input AddBook {
+        authors: [String]!
+        description: String!
+        bookId: ID!
+        title: String!
+        image: String
+        link: String
+    }
+    
+    type Mutation {
+        loginUser(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: AddBook!): User
+        removeBook(bookId: ID!): User
+    }`
